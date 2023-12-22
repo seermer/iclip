5d99fe8ed472ea59c1ea5273ad331317a5e7f5f9
Zhantao Yang <mervynyang26@gmail.com>
2023-12-22 12:28:18 -0500

    debug

--- tools/train.py
+++ tools/train.py
@@ -20,20 +20,15 @@ def seed_all(seed: int = 1):
     os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-    import numpy
-    import torch, random
-    from torch.backends import cudnn
+    import torch, random, numpy
 
     random.seed(seed)
     numpy.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    cudnn.benchmark = False
-    cudnn.deterministic = True
     # torch.set_deterministic_debug_mode('warn')
     # torch.use_deterministic_algorithms(True, warn_only=True)
-    print('DETERMINISTIC SET')
 
 
 def parse_args():
