d8b4e96da4925cff7daa16dbcf69712cb1284b76
Zhantao Yang <mervynyang26@gmail.com>
2023-12-29 14:53:11 -0500

    add skeleton for visualization

--- visualize.py
+++ visualize.py
@@ -4,7 +4,8 @@ from mmengine import Config
 
 def main():
     cfg = Config.fromfile('./configs/1ours_rcnn/base_rescale_collage+.py')
-    dataloader = Runner.build_dataloader(cfg.train_dataloader)
+    runner = Runner.from_cfg(cfg)
+    dataloader = runner.build_dataloader(cfg.train_dataloader)
     for batch in dataloader:
         print(batch)
         print(type(batch))
