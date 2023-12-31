a1ba5fcc701c5b824b9ce4788a225effa9b612fa
Zhantao Yang <mervynyang26@gmail.com>
2023-12-31 18:50:59 -0500

    add sigmoid based model and config

--- configs/1ours_rcnn/focal_sigmoid_pretrain.py
+++ configs/1ours_rcnn/focal_sigmoid_pretrain.py
@@ -2,9 +2,10 @@ _base_ = 'base_rcnn_pretrain.py'
 
 model = dict(
     roi_head=dict(
-            bbox_head=dict(
-                loss_cls=dict(
-                    _delete_=True, type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=2.0)
-            )
+        bbox_head=dict(
+            type='IclipShared2FCBBoxHeadSigmoid',
+            loss_cls=dict(
+                _delete_=True, type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=2.0)
+        )
     )
 )
