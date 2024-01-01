ad41c6b314691e26f03eea639ec97b943fe410c7
Zhantao Yang <mervynyang26@gmail.com>
2024-01-01 02:32:43 -0500

    modify loss weight

--- configs/1ours_rcnn/sigmoid_pretrain.py
+++ configs/1ours_rcnn/sigmoid_pretrain.py
@@ -4,7 +4,7 @@ model = dict(
     roi_head=dict(
         bbox_head=dict(
             loss_cls=dict(
-                _delete_=True, type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0 / 50.
+                _delete_=True, type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0 / 8.
             )
         )
     )
