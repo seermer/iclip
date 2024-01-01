d51d297a5e5483a1ad59c82a0cb69035e7d300e0
Zhantao Yang <mervynyang26@gmail.com>
2023-12-31 22:19:15 -0500

    add more configs

--- configs/1ours_rcnn/sigmoid_pretrain_nonorm_nobg.py
+++ configs/1ours_rcnn/sigmoid_pretrain_nonorm_nobg.py
@@ -2,7 +2,9 @@ _base_ = 'base_rcnn_pretrain.py'
 
 model = dict(
     roi_head=dict(
+        type='IclipRoIHeadSigmoid',
         bbox_head=dict(
+            type='IclipShared2FCBBoxHeadSigmoid',
             loss_cls=dict(
                 _delete_=True, type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)
         )
