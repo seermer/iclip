e0c7edb77fd58f05433c9248ca7aaa831c2ed5b2
Zhantao Yang <mervynyang26@gmail.com>
2023-12-31 22:04:57 -0500

    reduce loss weight fir sigmoid based

--- configs/1ours_rcnn/focal_sigmoid_pretrain.py
+++ configs/1ours_rcnn/focal_sigmoid_pretrain.py
@@ -5,7 +5,7 @@ model = dict(
         bbox_head=dict(
             type='IclipShared2FCBBoxHeadSigmoid',
             loss_cls=dict(
-                _delete_=True, type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=2.0)
+                _delete_=True, type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=1.0)
         )
     )
 )
