08d1323c5de70b9acef2e8d4e6430fcf2a6da25a
Zhantao Yang <mervynyang26@gmail.com>
2024-01-02 16:35:38 -0500

    debugging config

--- configs/1ours_rcnn/test_focal_pretrain.py
+++ configs/1ours_rcnn/test_focal_pretrain.py
@@ -15,7 +15,7 @@ model = dict(
             nms_thr=0.7,
             min_bbox_size=0),
         rcnn=dict(
-            score_thr=0.05,
+            score_thr=0.03,
             nms=dict(type='nms', iou_threshold=0.5, class_agnostic=True),
             max_per_img=100))
 )
