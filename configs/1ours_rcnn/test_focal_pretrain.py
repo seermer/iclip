bdef7f618525d2a870c6509eee73ce1c47ead851
Zhantao Yang <mervynyang26@gmail.com>
2024-01-02 16:16:20 -0500

    increase conf threshold

--- configs/1ours_rcnn/test_focal_pretrain.py
+++ configs/1ours_rcnn/test_focal_pretrain.py
@@ -15,9 +15,9 @@ model = dict(
             nms_thr=0.7,
             min_bbox_size=0),
         rcnn=dict(
-            score_thr=0.01,
+            score_thr=0.3,
             nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=1000))
+            max_per_img=100))
 )
 
 val_pipeline = [
