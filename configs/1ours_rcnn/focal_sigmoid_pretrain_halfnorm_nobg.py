61230279201ee28a2df78a8dd196aef495aa7a11
Zhantao Yang <mervynyang26@gmail.com>
2024-01-02 19:01:03 -0500

    add new config

--- configs/1ours_rcnn/focal_sigmoid_pretrain_halfnorm_nobg.py
+++ configs/1ours_rcnn/focal_sigmoid_pretrain_halfnorm_nobg.py
@@ -6,7 +6,7 @@ model = dict(
             type='IclipShared2FCBBoxHeadSigmoid',
             loss_cls=dict(
                 _delete_=True, type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25,
-                loss_weight=2.0
+                loss_weight=2.0 / 8.
             )
         )
     )
