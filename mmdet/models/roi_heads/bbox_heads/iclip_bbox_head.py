5a8ac1c80e09d125c8e8b402e853408c5cc9341e
Zhantao Yang <mervynyang26@gmail.com>
2024-01-03 15:32:43 -0500

    add more debug info

--- mmdet/models/roi_heads/bbox_heads/iclip_bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/iclip_bbox_head.py
@@ -144,8 +144,9 @@ class IclipBBoxHead(BBoxHead):
                     losses.update(loss_cls_)
                 else:
                     losses['loss_cls'] = loss_cls_
-                print_log(cls_score.argmax(-1)[:20], 'current')
-                print_log(labels[:20], 'current')
+                print_log(cls_score.max(-1)[:10], 'current')
+                print_log(cls_score.argmax(-1)[:10], 'current')
+                print_log(labels[:10], 'current')
                 if self.custom_activation:
                     acc_ = self.loss_cls.get_accuracy(cls_score, labels)
                     losses.update(acc_)
