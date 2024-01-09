19ee89310244f39d28dd1076256917683a12dbb7
Zhantao Yang <mervynyang26@gmail.com>
2024-01-09 00:46:00 -0500

    slightly change base config

---
+++ configs/1ours_rcnn/pretrain_1ratio.py
@@ -0,0 +1,11 @@
+_base_ = 'base_rcnn_pretrain.py'
+
+model = dict(
+    train_cfg=dict(
+        rcnn=dict(
+            sampler=dict(
+                add_gt_as_proposals=True,
+                neg_pos_ub=1.,
+                num=512,
+                pos_fraction=0.5,
+                type='RandomSampler'))))
