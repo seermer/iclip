ca2679e08499d6ae3e3c3aceb4c4b5b962537f4a
Zhantao Yang <mervynyang26@gmail.com>
2024-01-09 22:51:34 -0500

    update loss weight

--- configs/1ours_rcnn/sigmoid_pretrain_1ratio.py
+++ configs/1ours_rcnn/sigmoid_pretrain_1ratio.py
@@ -1,13 +1,10 @@
 _base_ = 'pretrain_1ratio.py'
 
-grid = list(range(2, 10))
-weight = sum(x ** 2 for x in grid) / len(grid)
-
 model = dict(
     roi_head=dict(
         bbox_head=dict(
             loss_cls=dict(
-                _delete_=True, type='CrossEntropyLoss', use_sigmoid=True, loss_weight=weight
+                _delete_=True, type='CrossEntropyLoss', use_sigmoid=True, loss_weight=10.
             )
         )
     )
