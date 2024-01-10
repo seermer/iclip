d84e5e3ea58f297a0879e5d94cc2372caa23561b
Zhantao Yang <mervynyang26@gmail.com>
2024-01-09 22:46:31 -0500

    update loss weight

--- configs/1ours_rcnn/sigmoid_pretrain_nonorm_1ratio.py
+++ configs/1ours_rcnn/sigmoid_pretrain_nonorm_1ratio.py
@@ -1,13 +1,10 @@
-_base_ = 'pretrain_1ratio.py'
+_base_ = 'sigmoid_pretrain_1ratio.py'
 
 model = dict(
     roi_head=dict(
         type='IclipRoIHeadSigmoid',
         bbox_head=dict(
             type='IclipShared2FCBBoxHeadSigmoid',
-            loss_cls=dict(
-                _delete_=True, type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0 / 50.
-            )
         )
     )
 )
