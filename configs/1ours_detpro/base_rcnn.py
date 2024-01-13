68ccd65e78a759b55baec31717a499e1996b3c37
Zhantao Yang <mervynyang26@gmail.com>
2024-01-13 14:17:25 -0500

    add debug config

--- configs/1ours_detpro/base_rcnn.py
+++ configs/1ours_detpro/base_rcnn.py
@@ -99,11 +99,15 @@ model = dict(
         rcnn=dict(
             assigner=dict(
                 type='MaxIoUAssigner',
-                pos_iou_thr=0.5,
-                neg_iou_thr=0.5,
-                min_pos_iou=0.5,
                 match_low_quality=True,
-                ignore_iof_thr=-1)
+                ignore_iof_thr=-1
+            ),
+            sampler=dict(
+                add_gt_as_proposals=True,
+                neg_pos_ub=1.,
+                num=512,
+                pos_fraction=0.5,
+                type='RandomSampler')
         )
     )
 )
