38d6b7fbcbdb752e39851c2ca7d483c08c812bef
zhantaoy <mervynyang26@gmail.com>
2024-01-19 11:21:45 -0500

    change lr and bs for new config

--- configs/1ours_detpro/lamb_bs2x_negpos1x.py
+++ configs/1ours_detpro/lamb_bs2x_negpos1x.py
@@ -1,9 +1,9 @@
 _base_ = 'lamb_negpos1x.py'
 
 train_dataloader = dict(
-    batch_size=36
+    batch_size=32
 )
 
 optim_wrapper = dict(
-    optimizer=dict(lr=0.04)
+    optimizer=dict(lr=0.035)
 )
